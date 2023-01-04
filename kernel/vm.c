3848ad5d2ab331842e8491b84a3e05357df3120a
Clouder0 <clouder0@outlook.com>
2023-01-04 17:39:41 +0800

    Finished print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint_walk(pagetable_t pagetable, int depth){
+  if(depth == 3) return;
+  for(int i = 0; i < 512; ++i)
+  {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) == 0)
+      continue;
+    for (int i = 0; i < depth; ++i)
+      printf(".. ");
+    printf("..%d: pte %p pa %p\n", i, pte, pagetable);
+    if ((pte & PTE_V) && (pte & (PTE_R | PTE_W | PTE_X)) == 0)
+    {
+      uint64 child = PTE2PA(pte);
+      vmprint_walk((pagetable_t)child, depth + 1);
+    }
+  }
+}
+
+// Print a page table
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_walk(pagetable, 0);
+}
